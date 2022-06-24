62b1833a41d5a13e5a1aff73fa6dc8d19e9f8af7
José Abelardo Vicente Filho <55700120+maxabelardo@users.noreply.github.com>
2022-06-24 17:02:15 -0300

    Create Analise dos dados do map.sql

---
+++ scripts_exemplos/Analise dos dados do map.sql
@@ -0,0 +1,134 @@
+/*
+Fiz o export dos dados da planilha de Excel para o banco de dados no servidor 2611 
+
+*/
+
+--Cruzando os dados o MAP com o CSSM e AD.
+
+SELECT DISTINCT
+       LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+      ,[Computer Name]
+	  ,[HostName] AS 'SCCM'
+	  ,A.[Name]  AS 'ActiveDirectory'
+      ,[SQL Server Instance Name]
+      ,[SQL Server Product Name]
+      ,[SQL Server Version]
+      ,[SQL Server Service Pack]
+      ,[SQL Server Edition]
+      ,[Clustered?]
+      ,[SQL Server Cluster Network Name]
+      ,[SQL Service State]
+      ,[SQL Service Start Mode]
+      ,[Language]
+      ,[SQL Server Sub-Directory]
+      ,[Current Operating System]
+      ,[Operating System Service Pack Level]
+      ,[Operating System Architecture Type]
+      ,[Number of Processors]
+      ,[Number of Total Cores]
+      ,[Number of Logical Processors]
+      ,[CPU]
+      ,[System Memory (MB)]
+      ,[Logical Disk Drive Name]
+      ,[Logical Disk Size (GB)]
+      ,[Logical Disk Free Space (GB)]
+      ,[Machine Type]
+      ,[Number of Host Processors]
+      ,[WMI Status]
+  FROM [dbo].[DatabaseInstances] AS S
+  LEFT JOIN [dbo].[VW_ServerHost] AS H ON H.[HostName] COLLATE DATABASE_DEFAULT  = LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+ LEFT JOIN [DBActiveDirectory].[AD].[STGADComputer] AS A ON A.[Name] COLLATE DATABASE_DEFAULT  = LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+WHERE [HostName] IS NOT NULL
+ 
+GO
+
+
+USE [SCCM_view]
+GO
+
+SELECT DISTINCT
+       LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+      ,[Computer Name]
+	  ,[HostName] AS 'SCCM'
+	  ,A.[Name]  AS 'ActiveDirectory'
+      ,[SQL Server Instance Name]
+      ,[SQL Server Product Name]
+      ,[SQL Server Version]
+      ,[SQL Server Service Pack]
+      ,[SQL Server Edition]
+      ,[Clustered?]
+      ,[SQL Server Cluster Network Name]
+      ,[SQL Service State]
+      ,[SQL Service Start Mode]
+      ,[Language]
+      ,[SQL Server Sub-Directory]
+      ,[Current Operating System]
+      ,[Operating System Service Pack Level]
+      ,[Operating System Architecture Type]
+      ,[Number of Processors]
+      ,[Number of Total Cores]
+      ,[Number of Logical Processors]
+      ,[CPU]
+      ,[System Memory (MB)]
+      ,[Logical Disk Drive Name]
+      ,[Logical Disk Size (GB)]
+      ,[Logical Disk Free Space (GB)]
+      ,[Machine Type]
+      ,[Number of Host Processors]
+      ,[WMI Status]
+  FROM [dbo].[DatabaseInstances] AS S
+  LEFT JOIN [dbo].[VW_ServerHost] AS H ON H.[HostName] COLLATE DATABASE_DEFAULT  = LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+  LEFT JOIN [DBActiveDirectory].[AD].[STGADComputer] AS A ON A.[Name] COLLATE DATABASE_DEFAULT  = LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+WHERE [HostName] IS NULL
+GO
+
+
+--------------------------------------------------------
+--Extraindo o quantitativos
+
+USE [SCCM_view]
+GO
+
+SELECT DISTINCT COUNT(*) 'Exist no SCCM e no AD.'
+       /*LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+      ,[Computer Name]
+	  ,[HostName] AS 'SCCM'
+	  ,A.[Name]  AS 'ActiveDirectory'*/
+  FROM [dbo].[DatabaseInstances] AS S
+  LEFT JOIN [dbo].[VW_ServerHost] AS H ON H.[HostName] COLLATE DATABASE_DEFAULT  = LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+ LEFT JOIN [DBActiveDirectory].[AD].[STGADComputer] AS A ON A.[Name] COLLATE DATABASE_DEFAULT  = LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+WHERE [HostName] IS NOT NULL AND A.[Name] IS NOT NULL
+
+SELECT DISTINCT COUNT(*) AS 'Não existe nem no SCCM nem no AD.' /*
+       LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+      ,[Computer Name]
+	  ,[HostName] AS 'SCCM'
+	  ,A.[Name]  AS 'ActiveDirectory'*/
+  FROM [dbo].[DatabaseInstances] AS S
+  LEFT JOIN [dbo].[VW_ServerHost] AS H ON H.[HostName] COLLATE DATABASE_DEFAULT  = LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+  LEFT JOIN [DBActiveDirectory].[AD].[STGADComputer] AS A ON A.[Name] COLLATE DATABASE_DEFAULT  = LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+WHERE [HostName] IS NULL AND A.[Name] IS NULL
+
+--------------------------------
+
+SELECT DISTINCT COUNT(*) AS 'Existe apenas no SCCM'
+       /*LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+      ,[Computer Name]
+	  ,[HostName] AS 'SCCM'
+	  ,A.[Name]  AS 'ActiveDirectory'*/
+  FROM [dbo].[DatabaseInstances] AS S
+  LEFT JOIN [dbo].[VW_ServerHost] AS H ON H.[HostName] COLLATE DATABASE_DEFAULT  = LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+ LEFT JOIN [DBActiveDirectory].[AD].[STGADComputer] AS A ON A.[Name] COLLATE DATABASE_DEFAULT  = LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+WHERE [HostName] IS NOT NULL AND A.[Name] IS NULL
+
+SELECT DISTINCT COUNT(*) AS 'Existe apenas no AD.'/*
+       LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+      ,[Computer Name]
+	  ,[HostName] AS 'SCCM'
+	  ,A.[Name]  AS 'ActiveDirectory'*/
+  FROM [dbo].[DatabaseInstances] AS S
+  LEFT JOIN [dbo].[VW_ServerHost] AS H ON H.[HostName] COLLATE DATABASE_DEFAULT  = LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+  LEFT JOIN [DBActiveDirectory].[AD].[STGADComputer] AS A ON A.[Name] COLLATE DATABASE_DEFAULT  = LEFT([Computer Name], (CHARINDEX('.',[Computer Name])-1) )
+WHERE [HostName] IS NULL AND A.[Name] IS NOT NULL
+
+
